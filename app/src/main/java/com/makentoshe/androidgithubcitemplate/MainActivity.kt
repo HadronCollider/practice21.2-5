4dc284baf4085de0eaa7211593d96b927c4b1534
inngutorova <inngutorova@gmail.com>
2021-06-18 00:35:15 +0300

    Added Charts and Archive button, ChartsActivity and ArchiveActivity

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.makentoshe.androidgithubcitemplate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bCharts = findViewById<ImageButton>(R.id.buttonCharts)
+
+        bCharts.setOnClickListener{
+            val intent = Intent(this, ChartsActivity::class.java)
+            startActivity(intent)
+        }
+
+        val bArchive = findViewById<ImageButton>(R.id.buttonArchive)
+
+        bArchive.setOnClickListener{
+            val intent = Intent(this, ArchiveActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
 }
