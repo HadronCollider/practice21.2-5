54a2b6c4cac2fbf41b3befde62f9fe91707758aa
t0nso1eil <katekatekd@mail.ru>
2021-06-18 11:25:48 +0300

    Charts Activity without home button

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/ChartsActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/ChartsActivity.kt
@@ -1,11 +1,20 @@
 package com.makentoshe.androidgithubcitemplate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class ChartsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_charts)
+
+        val bArchive = findViewById<ImageButton>(R.id.buttonArchive)
+
+        bArchive.setOnClickListener{
+            val intent = Intent(this, ArchiveActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
