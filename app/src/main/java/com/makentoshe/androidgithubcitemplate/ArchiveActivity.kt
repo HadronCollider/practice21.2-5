bd09a40fa705b86023dfcf1a7ec77a7b2f8a186c
TushaChesnokova <angarlic@mail.ru>
2021-06-18 10:50:07 +0300

    Добаблены в архив логотип и кнопка чартов

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/ArchiveActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/ArchiveActivity.kt
@@ -1,11 +1,20 @@
 package com.makentoshe.androidgithubcitemplate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class ArchiveActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_archive)
+
+        val bCharts = findViewById<ImageButton>(R.id.buttonCharts)
+
+        bCharts.setOnClickListener{
+            val intent = Intent(this, ChartsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
